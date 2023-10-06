959c24ffee9db8aae5eb392d21998908fa313224
Thinh Diep Tu <dtthinh@tma.com.vn>
2023-10-06 17:22:55 +0700

    push the header basic

--- ReactJS/tiktok/src/components/Layout/components/Header/index.js
+++ ReactJS/tiktok/src/components/Layout/components/Header/index.js
@@ -1,12 +1,31 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss'
+import images from '~/assets/images';
 
 // bind object style roi tra ra 1 method la 1 function cx
 // dung cx de dung class => giup viet dc classname duoi dang dau -
 const cx = classNames.bind(styles)
 
 function Header() {
-    return <header className={cx('wrapper')}><div className={cx('inner')}></div></header> 
+    return <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+    </header> 
         
 }
 
